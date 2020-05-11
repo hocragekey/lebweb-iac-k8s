231b69059e76d008d7458f5ea3db9b828e4b4bff
James Lebonitte <jlebonitte@gmail.com>
2020-05-11 04:56:26 -0400

    added missing azurerm provider block

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
 resource "azurerm_resource_group" "spark-k8s" {
   name     = "${var.k8s_resource_group_name}"
   location = "${var.location}"
